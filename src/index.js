edf4ce96db176a05274e32bf5c8dbd0d9b0dcf4d
Mahdi Noori <mahdinoori2389@gmail.com>
2023-07-04 17:37:45 +0430

    Add functionality for displying the movies in project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import { fetchData } from './modules/fetchAPI.js';
+
+// for (let i = 1; i <= 12; i += 1) {
+//   fetchData(i);
+// }
+fetchData();
---
+++ src/modules/fetchAPI.js
@@ -0,0 +1,32 @@
+const baseUrl = 'https://api.tvmaze.com/search/shows?q=war';
+const container = document.querySelector('.container');
+const movieNum = document.getElementById('movie-number');
+
+export const fetchData = async () => {
+  try {
+    const response = await fetch(baseUrl);
+    const data = await response.json();
+    const code = data.map((movie) => `
+    <ul class="movie" id="${movie.show.id}">
+    <li class="overflow">
+      <img src="${movie.show.image.original}" alt="girl picture" class="movie-img" />
+    </li>
+    <li class="title-like">
+    <h2 class="movie-name">${movie.show.name}</h2>
+    <button class="like-btn">Like</button>
+  </li>
+  <li><p class="likes">0 Likes</p></li> 
+  <li class="comment-reservation">
+    <button class="comment-btn">Comment</button>
+    <button class="reservation-btn">reservation</button>
+  </li>
+  </ul>
+    `);
+    container.innerHTML = code;
+    movieNum.innerHTML = data.length;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default fetchData;
