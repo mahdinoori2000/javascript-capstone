d12549b99aa3531f4e702300a48694ff4ce6bf5e
H-Ramalan <talktoramalan@gmail.com>
2023-07-06 10:51:32 -0700

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
-import './style.css';
-import { fetchData } from './modules/fetchAPI.js';
-import commentBtnEventListener from './modules/comment.js';
+import "./style.css";
+import { fetchData } from "./modules/fetchAPI.js";
+import commentBtnEventListener from "./modules/comment.js";
 
 await fetchData();
 commentBtnEventListener();
-
-// const fetchComments = async (itemId) => {
-//   try {
-//     const response = await fetch(
-//       `https://api.involvement-api.com/items/${itemId}/comments`
-//     );
-//     if (!response.ok) {
-//       throw new Error("Failed to fetch comments.");
-//     }
-//     const data = await response.json();
-//     return data.comments;
-//   } catch (error) {
-//     console.error(error);
-//     return [];
-//   }
-// };
-
-// export default fetchComments;
