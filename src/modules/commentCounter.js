21c4441b97ea32c8bd6e5efac2d03cf3800b3449
H-Ramalan <talktoramalan@gmail.com>
2023-07-07 13:57:25 -0700

    add comment ounter function

--- src/modules/commentCounter.js
+++ src/modules/commentCounter.js
@@ -1,4 +1,4 @@
-import commentBaseUrl from "./api.js";
+import commentBaseUrl from './api.js';
 
 const countComments = async (id) => {
   const params = {
@@ -10,39 +10,4 @@ const countComments = async (id) => {
   return result.length;
 };
 
-export { countComments };
-
-// import { countComments } from "../modules/commentCount.js";
-
-// global.fetch = jest.fn(() =>
-//   Promise.resolve({
-//     json: () =>
-//       Promise.resolve([
-//         {
-//           username: "Salim",
-//           creation_date: "2023-06-24",
-//           comment: "Good show",
-//         },
-//         {
-//           creation_date: "2022-04-11",
-//           username: "Hamza",
-//           comment: "Bad review",
-//         },
-//         {
-//           creation_date: "2020-04-05",
-//           username: "Mahdi",
-//           comment: "Great movie",
-//         },
-//       ]),
-//   })
-// );
-// describe("comment counter tests using Jest", () => {
-//   test("test Comment Count ", async () => {
-//     const response = await countComments(3);
-//     expect(response.length).toBe(3);
-//   });
-//   test("Wrong test Comment Count", async () => {
-//     const response = await countComments(3);
-//     expect(response.length).not.toBe(1);
-//   });
-// });
+export default countComments;
